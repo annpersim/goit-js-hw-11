9386fbe8b79f568ad76b0cbd9169c75ba5987833
annpersim <annasharonova767@gmail.com>
2023-01-29 15:51:41 +0300

    js hw 11

---
+++ src/js/fetchPixabay.js
@@ -0,0 +1,47 @@
+import axios from 'axios';
+import Notiflix from 'notiflix';
+
+const URL = 'https://pixabay.com/api/';
+const API_KEY = '33152834-eeaeeae4e604e04dc4cc38ec8';
+
+export default class PixabayApi {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+  }
+
+  async fetchPixabay() {
+    const options = {
+      key: `${API_KEY}`,
+      q: `${this.searchQuery}`,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+      per_page: 40,
+      page: `${this.page}`,
+    };
+
+    const response = await axios.get(URL, { params: options });
+
+    if (response.data.totalHits === 0) {
+      return Notiflix.Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+    } else if (response.data.totalHits < options.per_page * `${this.page}`) {
+      return Notiflix.Notify.failure(
+        "We're sorry, but you've reached the end of search results."
+      );
+    } else {
+      this.incrementPage();
+      return response.data;
+    }
+  }
+
+  incrementPage() {
+    this.page += 1;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+}
---
+++ src/js/index.js
@@ -0,0 +1,115 @@
+import PixabayApi from './fetchPixabay';
+import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const refs = {
+  searchForm: document.querySelector('#search-form'),
+  input: document.querySelector('input'),
+  submitBtn: document.querySelector('button'),
+  galleryDiv: document.querySelector('.gallery'),
+};
+
+const newPixabayApi = new PixabayApi();
+var lightbox = new SimpleLightbox('.gallery a', {
+  captions: false,
+});
+
+refs.searchForm.addEventListener('submit', onSearch);
+
+async function onSearch(e) {
+  e.preventDefault();
+  clearGallery();
+  newPixabayApi.resetPage();
+
+  newPixabayApi.searchQuery = e.target.searchQuery.value;
+
+  const image = await newPixabayApi.fetchPixabay();
+  const images = image.hits;
+  addImageToGallery(images);
+
+  const totalHits = image.totalHits;
+  Notiflix.Notify.success('Hooray! We found ' + `${totalHits}` + ' images.');
+}
+
+function addImageToGallery(images) {
+  refs.galleryDiv.insertAdjacentHTML(
+    'beforeend',
+    images
+      .map(
+        ({
+          largeImageURL,
+          webformatURL,
+          tags,
+          likes,
+          views,
+          comments,
+          downloads,
+        }) =>
+          `<div class="photo-card">
+            <div class="photo-image">
+              <a href="${largeImageURL}">
+                <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+              </a>
+            </div>
+            <div class="info">
+              <div class="info-item"><b>Likes</b><p class="info-value">${likes}</p></div>
+              <div class="info-item"><b>Views</b><p class="info-value">${views}</p></div>
+              <div class="info-item"><b>Comments</b><p class="info-value">${comments}</p></div>
+              <div class="info-item"><b>Downloads</b><p class="info-value">${downloads}</p></div>
+            </div>
+          </div>`
+      )
+      .join('')
+  );
+  lightbox.refresh();
+}
+
+function clearGallery() {
+  refs.galleryDiv.innerHTML = '';
+}
+
+//Infinite scroll
+
+(() => {
+  window.addEventListener('scroll', throttle(checkPosition, 300));
+  window.addEventListener('resize', throttle(checkPosition, 300));
+})();
+
+async function checkPosition() {
+  const height = document.body.offsetHeight;
+  const screenHeight = window.innerHeight;
+  const scrolled = window.scrollY;
+  const threshold = height - screenHeight / 4;
+  const position = scrolled + screenHeight;
+
+  if (position >= threshold) {
+    const image = await newPixabayApi.fetchPixabay();
+    const images = image.hits;
+    addImageToGallery(images);
+
+    const { height: cardHeight } = document
+      .querySelector('.gallery')
+      .firstElementChild.getBoundingClientRect();
+
+    window.scrollBy({
+      top: cardHeight * 2,
+      behavior: 'smooth',
+    });
+  }
+}
+
+function throttle(callee, timeout) {
+  let timer = null;
+
+  return function perform(...args) {
+    if (timer) return;
+
+    timer = setTimeout(() => {
+      callee(...args);
+
+      clearTimeout(timer);
+      timer = null;
+    }, timeout);
+  };
+}
